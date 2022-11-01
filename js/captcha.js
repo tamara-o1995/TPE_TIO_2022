609a35e3ca6ca89621e369612dc3ac071975e94e
Franco Delucchi <fdeluccho@gmail.com>
2022-11-01 00:04:38 -0300

    ultimos cambios agregados capctcha funcionando

--- js/captcha.js
+++ js/captcha.js
@@ -1,85 +1,90 @@
-"use strict";
+document.addEventListener("DOMContentLoaded", iniciarPagina);
 
-let random; 
-document.querySelector('#send').addEventListener("click", verificarFormulario);
+function iniciarPagina() {
 
-generarCaptcha(); 
+    "use strict";
 
-function generarCaptcha() {
-    random = Math.floor(Math.random() * (900000) + 100000);
-    document.querySelector('#captcha').innerHTML = random; 
-}
+    let random; 
+    document.querySelector('#send').addEventListener("click", verificarFormulario);
 
-function verificarFormulario(event) {
-    event.preventDefault()
+    generarCaptcha(); 
 
-    let error = 0;
-    let name = document.querySelector('#name');
-    let lastName = document.querySelector('#lastName');
-    let email = document.querySelector('#email');
-    let query = document.querySelector('#query');
-    let msg = document.querySelector('#msg');
-    let age = document.querySelector('#age');
-    let adress = document.querySelector('#adress');
-    let type = document.querySelector('#type');
-    let msgSend = document.querySelector('#msgSend');
-    let captchaInput = document.querySelector('#captchaInput');
-    msgSend.innerHTML = ''; 
-
-    if (name.value == "") {
-        name.classList.add("error");
-        error = 1;
-    } else { 
-        name.classList.remove("error");
-    }
-    if (lastName.value == "") {
-        lastName.classList.add("error");
-        error = 1;
-    } else {
-        lastName.classList.remove("error");
-    }
-    if (email.value == "") {
-        email.classList.add("error");
-        error = 1;
-    } else {
-        email.classList.remove("error");
-    }
-    if (query.value == "") {
-        query.classList.add("error");
-        error = 1;
-    } else {
-        query.classList.remove("error");
+    function generarCaptcha() {
+        random = Math.floor(Math.random() * (900000) + 100000);
+        document.querySelector('#captcha').innerHTML = random; 
     }
 
-    if (error == 1) { 
-        generarCaptcha();
-        msg.innerHTML = "Campos incompletos";
-    }
-    else if (captchaInput.value != random) { 
+    function verificarFormulario(event) {
+        event.preventDefault()
 
-        name.classList.remove("error");
-        lastName.classList.remove("error");
-        email.classList.remove("error");
-        query.classList.remove("error");
-        captchaInput.classList.add("error");
-        msg.innerHTML = "Captcha incorrecto";
-        generarCaptcha();
-    } else {
-        name.classList.remove("error");
-        lastName.classList.remove("error");
-        email.classList.remove("error");
-        query.classList.remove("error");
-        captchaInput.classList.remove("error");
-        name.value = '';
-        lastName.value = '';
-        email.value = '';
-        query.value = '';
-        captchaInput.value = '';
-        msg.innerHTML = '';
-        age.value = '';
-        adress.value = '';
-        type.value = '';
-        msgSend.innerHTML = "¡Gracias por su consulta!";
-        generarCaptcha();
+        let error = 0;
+        let name = document.querySelector('#name');
+        let lastName = document.querySelector('#lastName');
+        let email = document.querySelector('#email');
+        let query = document.querySelector('#query');
+        let msg = document.querySelector('#msg');
+        let age = document.querySelector('#age');
+        let adress = document.querySelector('#adress');
+        let type = document.querySelector('#type');
+        let msgSend = document.querySelector('#msgSend');
+        let captchaInput = document.querySelector('#captchaInput');
+        msgSend.innerHTML = ''; 
+
+        if (name.value == "") {
+            name.classList.add("error");
+            error = 1;
+        } else { 
+            name.classList.remove("error");
+        }
+        if (lastName.value == "") {
+            lastName.classList.add("error");
+            error = 1;
+        } else {
+            lastName.classList.remove("error");
+        }
+        if (email.value == "") {
+            email.classList.add("error");
+            error = 1;
+        } else {
+            email.classList.remove("error");
+        }
+        if (query.value == "") {
+            query.classList.add("error");
+            error = 1;
+        } else {
+            query.classList.remove("error");
+        }
+
+        if (error == 1) { 
+            generarCaptcha();
+            msg.innerHTML = "Campos incompletos";
+        }
+        else if (captchaInput.value != random) { 
+
+            name.classList.remove("error");
+            lastName.classList.remove("error");
+            email.classList.remove("error");
+            query.classList.remove("error");
+            captchaInput.classList.add("error");
+            msg.innerHTML = "Captcha incorrecto";
+            generarCaptcha();
+        } else {
+            name.classList.remove("error");
+            lastName.classList.remove("error");
+            email.classList.remove("error");
+            query.classList.remove("error");
+            captchaInput.classList.remove("error");
+            name.value = '';
+            lastName.value = '';
+            email.value = '';
+            query.value = '';
+            captchaInput.value = '';
+            msg.innerHTML = '';
+            age.value = '';
+            adress.value = '';
+            type.value = '';
+            msgSend.innerHTML = "¡Gracias por su consulta!";
+            generarCaptcha();
+        }
     }
 }
