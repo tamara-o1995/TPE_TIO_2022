ce0d5e57c59f9c68ccef6ebf03faa4ffa9838920
mmolina727 <matiasgmolina.unicen@gmail.com>
2022-11-02 00:33:55 -0300

    Se agrega menu dinamico

---
+++ js/menujs.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", iniciarPagina);
+
+function iniciarPagina() { 
+
+    "use strict";
+    
+    document.querySelector(".btn_menu").addEventListener("click", toggleMenu);  
+
+    function toggleMenu() { 
+        document.querySelector(".navigation").classList.toggle("show");
+    }
+}
